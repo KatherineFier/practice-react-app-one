a2b94b3af783c5d3d585064dd0974c1288fdbf83
katherine_fier <katherinefier@Katherines-MacBook-Pro.local>
2024-01-31 16:55:43 -0700

    refactored app component and added body styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
+import {GlobalStyle} from './styles'
 
-function App() {
-  return (
+
+const App = () => {
+    const message = (name) => {
+      return `${name} is my dog.`
+    }
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle/>
+      <h1>Hello there!</h1>
+      <h2>{message`Lio`}</h2>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,16 @@
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+body{
+    color: #ffff;
+    height: 100vh;
+    width: 100vw;
+    margin:0;
+    padding: 0;
+    overflow: hidden;
+    text-rendering:optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    background: #bf9ded;
+    font-family: 'Raleway', sans-serif;
+    font-weight:500;
+}`
